dd90df6a395a65782dc45b499a73597a423a9938
Nicolas Maurin <nma@MacBook-Pro-de-Nicolas.local>
2020-11-15 23:29:58 +0100

    try catch sur la commande

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const Search = require('./class/search')
 
 bot.login('Nzc3NTIxODE3MjMyNDc0MTQz.X7Epng.ak3U-qkU4eq74xCeUVAtYgPjPaw');
 
-bot.on('message', function(message){
-    if (message.content.startsWith('/ks')){
-        let args = message.content.split(' ')
-        args.shift()
-        args.join()
-        let dataSearch = Search.searchMatch(args)
-        message.channel.send(
-            "> " + dataSearch.title,
-            { files: ["./sounds/" + dataSearch.file]}
-            );
-
+bot.on('message', function(message)
+    {
+        if (message.content.startsWith('/ks')){
+            let args = message.content.split(' ')
+            args.shift()
+            args.join()
+            let dataSearch = Search.searchMatch(args)
+            try {
+                message.channel.send(
+                    "> " + dataSearch.title,
+                    {files: ["./sounds/" + dataSearch.file]}
+                );
+            }
+            catch (error){
+                message.author.send('Pas de correspondances')
+            }
+        }
     }
-})
+)
 
 
